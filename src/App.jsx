94daccf45318fe56ea8758421c6a85663a46101a
fbtwitter <rzaugusdi354@gmail.com>
2022-06-24 12:49:08 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-screen h-screen">
       <Router>
         <Routes>
           <Route index path="/" element={<Home />} />
